65c6f0af605fc0f5438a1380bd004780248f976c
afruiz01 <141862421+afruiz01@users.noreply.github.com>
2024-02-18 18:43:26 -0500

    Add files via upload

---
+++ co.edu.uniquindio.solid/src/main/java/co/edu/uniquindio/solid/coche.java
@@ -0,0 +1,12 @@
+package co.edu.uniquindio.solid;
+
+public class coche {
+    class CocheDB{
+        void guardarCocheDB(responsabilidadUnica.Coche coche){  }
+        void eliminarCocheDB(responsabilidadUnica.Coche coche){}
+    }
+
+
+
+}
+
---
+++ co.edu.uniquindio.solid/src/main/java/co/edu/uniquindio/solid/responsabilidadUnica.java
@@ -0,0 +1,17 @@
+package co.edu.uniquindio.solid;
+
+public class responsabilidadUnica {
+    class Coche {
+        String marca;
+
+        Coche(String marca) {
+            this.marca = marca;
+        }
+
+        String getMarcaCoche() {
+            return marca;
+
+
+        }
+    }
+}
